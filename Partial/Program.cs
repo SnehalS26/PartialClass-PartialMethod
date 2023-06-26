e60c03a4971d4ba4f4db3492eae9e07203924941
SnehalS26 <Snehalshirsath26@gmail.com>
2023-06-26 17:41:20 +0530

    Add project files.

---
+++ Partial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc();
+            int c=calc.Addition(1, 1);
+            int c1= calc.Substraction(10,5);
+            int c2 = calc.Add(10, 5);
+            int c3 = calc.Multiply(10,5);
+            Console.WriteLine($"Addition = {c}");
+            Console.WriteLine($"Substraction = {c1}");
+            Console.WriteLine($"Add ={c2}");
+            Console.WriteLine($"Multiplication= {c3}");
+            calc.Display(20, 10);
+            
+        }
+    }
+}
---
+++ Partial/division.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial
+{
+    public partial class Calc
+    {
+        public int Multiply(int a, int b)
+        {
+            Sub(a, b);
+            return a * b;
+
+        }
+        // no need to write access specifier
+        // by default it is private, this method can not have any return type, only void is allowed
+        // we can use any other method to make a call to the partial method
+        partial void Sub(int a, int b)
+        {
+            Console.WriteLine($"Sub= { a - b}");
+        }
+        // helper method to give a call to sub
+        public void Display(int a, int b)
+        {
+            Sub(a, b);
+        }
+
+    }
+}
